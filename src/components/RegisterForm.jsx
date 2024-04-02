dae0b498cd8bf4919a844b2130db6d7a547dee1e
Martin <mmaina290@gmail.com>
2024-04-02 18:00:10 +0300

    added components for the register form

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,46 @@
+
+
+function RegisterForm() {
+  return (
+    <div className="wrapper">
+      <form id="register-form" action="/services" method="get">
+        <div className="form-box register">
+          <h2>Registration</h2>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="person"></ion-icon>
+            </span>
+            <input type="text" required title="Enter Username" id="register-username" />
+            <label>Username</label>
+          </div>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="mail"></ion-icon>
+            </span>
+            <input type="email" required title="Enter Email" id="register-email" />
+            <label>Email</label>
+          </div>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="lock-closed"></ion-icon>
+            </span>
+            <input type="password" required title="Enter Password" id="register-password" />
+            <label>Password</label>
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox"/>
+              I agree to the terms & conditions
+            </label>
+          </div>
+          <button type="submit" className="btn">Register</button>
+
+        </div>
+      </form>
+      
+    </div>
+  );
+  
+}
+
+export default RegisterForm;
