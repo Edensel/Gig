3355d08f9f770b7a3fc8919c4392ef387d7b4c3b
Martin <mmaina290@gmail.com>
2024-04-03 21:46:15 +0300

    updated app.lsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,41 @@ import FeatureSection from './components/FeatureSection';
 import Footer from './components/Footer';
 import Pricing from './components/Pricing';
 import Testimonials from './components/Testimonials';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    checkStorage();
+    return () => {};
+  }, [isLoggedIn]);
+  function checkStorage() {
+    if (localStorage.getItem("user")) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }
+  const logout = () => {
+    localStorage.removeItem("user");
+    setIsLoggedIn(false);
+  };
+
   return (
+    
     <Router>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
+        {isLoggedIn && <button onClick={logout}>Logout</button>}
         <Routes>
           <Route path="/" element={<HeroSection />} />
           <Route path="/features" element={<FeatureSection />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
         </Routes>
         <Footer />
       </div>
