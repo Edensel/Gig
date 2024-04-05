7723e958d42c13a9964e44e366f5180b7f7bdfbb
Densel Esekon <esekondensel@moringaschool.com>
2024-04-05 08:29:15 +0300

    Installed the latest version of node

--- src/App.jsx
+++ src/App.jsx
@@ -30,21 +30,24 @@
 // export default App;
 
 
-
-import React from 'react';
+import {useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeatureSection from './components/FeatureSection';
 import Pricing from './components/Pricing';
 import Testimonials from './components/Testimonials';
+import Footer from './components/Footer'; // Import Footer component
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     checkStorage();
     return () => {};
   }, [isLoggedIn]);
+
   function checkStorage() {
     if (localStorage.getItem("user")) {
       setIsLoggedIn(true);
@@ -52,13 +55,13 @@ const App = () => {
       setIsLoggedIn(false);
     }
   }
+
   const logout = () => {
     localStorage.removeItem("user");
     setIsLoggedIn(false);
   };
 
   return (
-    
     <Router>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
@@ -67,11 +70,13 @@ const App = () => {
           <Route path="/features" element={<FeatureSection />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/testimonials" element={<Testimonials />} />
+          <Route path='/login' element={<LoginForm/>}/>
         </Routes>
-        <Footer />
+        <Footer /> {/* Include the Footer component here */}
       </div>
     </Router>
   );
 };
 
 export default App;
+
