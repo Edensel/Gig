6470d6579da41a04213fc74066180b9a331c153f
Kevin <kevin.kiseli@student.moringaschool.com>
2024-04-05 21:32:12 +0300

    Added OrderPage to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,6 @@
 
 
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import HeroSection from './components/HeroSection';
-// import FeatureSection from './components/FeatureSection';
-// import Footer from './components/Footer';
-// import Pricing from './components/Pricing';
-// import Testimonials from './components/Testimonials';
-// import OrderPage from './components/OrderPage';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <div className="max-w-7xl mx-auto pt-20 px-6">
-//         <Routes>
-//           <Route path="/" element={<HeroSection />} />
-//           <Route path="/order" element={<OrderPage />} /> 
-//           <Route path="/features" element={<FeatureSection />} />
-//           <Route path="/pricing" element={<Pricing />} />
-//           <Route path="/testimonials" element={<Testimonials />} />
-//         </Routes>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
+// Gig/src/App.jsx
 
 import {useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -37,6 +9,7 @@ import HeroSection from './components/HeroSection';
 import FeatureSection from './components/FeatureSection';
 import Pricing from './components/Pricing';
 import Testimonials from './components/Testimonials';
+import OrderPage from './components/OrderPage';
 import Footer from './components/Footer'; // Import Footer component
 import LoginForm from './components/LoginForm'
 
@@ -67,6 +40,7 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Routes>
           <Route path="/" element={<HeroSection />} />
+          <Route path="/order" element={<OrderPage />} />
           <Route path="/features" element={<FeatureSection />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/testimonials" element={<Testimonials />} />
