571614aaf57e0afa93e1989efc590e31324c167d
Martin <mmaina290@gmail.com>
2024-04-07 23:57:36 +0300

    incorporated props and added signout and profile buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,12 @@
-import { useState} from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 import { Menu, X } from "lucide-react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
-import { Link } from "react-router-dom"; // Import Link component
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ isLoggedIn, handleSignOut }) => {
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
-
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // useEffect(() => {
-  //   checkStorage();
-  //   return () => {};
-  // }, [isLoggedIn]);
-  // function checkStorage() {
-  //   if (localStorage.getItem("user")) {
-  //     setIsLoggedIn(true);
-  //   } else {
-  //     setIsLoggedIn(false);
-  //   }
-  // }
-  // const logout = () => {
-  //   localStorage.removeItem("user");
-  //   setIsLoggedIn(false);
-  // };
-
- const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleSignIn = () => {
-    // Logic to handle user authentication
-    setIsLoggedIn(!isLoggedIn);
-  };
-
-  // const handleSignOut = () => {
-  //   // Logic to handle user sign out
-  //   setIsLoggedIn(false);
-  // };
 
   const toggleNavbar = () => {
     setMobileDrawerOpen(!mobileDrawerOpen);
@@ -49,7 +21,6 @@ const Navbar = () => {
             <span className="text-xl tracking-tight">Gig-Ahooy</span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
-            {/* Use Link components instead of anchor tags */}
             {navItems.map((item, index) => (
               <li key={index}>
                 <Link to={item.href}>{item.label}</Link>
@@ -57,22 +28,22 @@ const Navbar = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-          {/* {isLoggedIn && <button onClick={logout}>Logout</button>} */}
           {isLoggedIn ? (
-          <button className="py-2 px-3 border rounded-md"> {isLoggedIn ? "" : ""}
-            Sign Out
-          </button>
-        ) : (
-          <Link to="/login" className="py-2 px-3 border rounded-md" >  {isLoggedIn ? "Sign Out" : ""}
-                Sign In
-              </Link>
-        )}
-            <Link
-              to="/register"
-              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
-            >
-              Create an account
-            </Link>
+              <>
+                <button className="py-2 px-3 border rounded-md">Profile</button>
+                <button className="py-2 px-3 border rounded-md" onClick={handleSignOut}>Sign Out</button>
+              </>
+            ) : (
+              <>
+                <Link to="/login" className="py-2 px-3 border rounded-md">Sign In</Link>
+                <Link
+                  to="/register"
+                  className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+                >
+                  Create an account
+                </Link>
+              </>
+            )}
           </div>
           <div className="lg:hidden md:flex flex-col justify-end">
             <button onClick={toggleNavbar}>
@@ -83,7 +54,6 @@ const Navbar = () => {
         {mobileDrawerOpen && (
           <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
             <ul>
-              {/* Use Link components instead of anchor tags */}
               {navItems.map((item, index) => (
                 <li key={index} className="py-4">
                   <Link to={item.href}>{item.label}</Link>
@@ -91,26 +61,28 @@ const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-            <Link to="/login" className="py-2 px-3 border rounded-md">
-                Sign In
-              </Link>
-              <Link
-                to="/register"
-                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
-              >
-                Create an account
-              </Link>
+              {!isLoggedIn && (
+                <Link to="/login" className="py-2 px-3 border rounded-md">Sign In</Link>
+              )}
+              {!isLoggedIn && (
+                <Link
+                  to="/register"
+                  className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+                >
+                  Create an account
+                </Link>
+              )}
             </div>
           </div>
         )}
       </div>
-      
     </nav>
   );
 };
 
+Navbar.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  handleSignOut: PropTypes.func.isRequired,
+};
+
 export default Navbar;
-
-
-
-
