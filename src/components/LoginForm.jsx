18d9626892f3ecf72879084ee66959afe62b6868
Densel Esekon <densel.esekon@moringaschool.com>
2024-04-08 08:49:18 +0300

    Updated the forms functions && created api.jsx file for linking frontend and backend

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,25 @@
-
-import "../login.css";
-
+import React, { useState } from 'react';
+import '../login.css';
+import { loginUser } from './api';
 
 function LoginForm() {
- 
-  const handleSubmit = () => {
-    alert("Login Successful");
-  }
-  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await loginUser({ email, password });
+      alert(response.message);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+
   return (
     <div className="wrapper">
-      <form id="login-form" action ="/" method="get" >
+      <form id="login-form" onSubmit={handleSubmit}>
         <span className="icon-close">
           <ion-icon name="close"></ion-icon>
         </span>
@@ -20,24 +29,37 @@ function LoginForm() {
             <span className="icon">
               <ion-icon name="mail"></ion-icon>
             </span>
-            <input type="email" required title="Enter Email" id="login-email" />
+            <input
+              type="email"
+              required
+              title="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <label>Email</label>
           </div>
           <div className="input-box">
             <span className="icon">
               <ion-icon name="lock-closed"></ion-icon>
             </span>
-            <input type="password" required title="Enter password" id="login-password" />
+            <input
+              type="password"
+              required
+              title="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <label>Password</label>
           </div>
           <div className="login-forgot">
             <label>
-              <input type="checkbox" />
-              Remember Me!
+              <input type="checkbox" /> Remember Me!
             </label>
-            <a href="#"> Forgot Password </a>
+            <a href="#">Forgot Password</a>
           </div>
-          <button type="submit" className="btn" onClick={handleSubmit}>Login</button>
+          <button type="submit" className="btn">
+            Login
+          </button>
         </div>
       </form>
     </div>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,51 +1,73 @@
-// import {register} from "..constants";
+import React, { useState } from 'react';
+import { registerUser } from './api';
 
 function RegisterForm() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {
-    alert("Registration Successful");
-  }
-  
-    return (
-      <div className="wrapper">
-        <form id="register-form" action="/" method="get">
-          <div className="form-box register">
-            <h2>Registration</h2>
-            <div className="input-box">
-              <span className="icon">
-                <ion-icon name="person"></ion-icon>
-              </span>
-              <input type="text" required title="Enter Username" id="register-username" />
-              <label>Username</label>
-            </div>
-            <div className="input-box">
-              <span className="icon">
-                <ion-icon name="mail"></ion-icon>
-              </span>
-              <input type="email" required title="Enter Email" id="register-email" />
-              <label>Email</label>
-            </div>
-            <div className="input-box">
-              <span className="icon">
-                <ion-icon name="lock-closed"></ion-icon>
-              </span>
-              <input type="password" required title="Enter Password" id="register-password" />
-              <label>Password</label>
-            </div>
-            <div className="remember-forgot">
-              <label>
-                <input type="checkbox"/>
-                I agree to the terms & conditions
-              </label>
-            </div>
-            <button type="submit" className="btn" onClick={handleSubmit}>Register</button>
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await registerUser({ username, email, password });
+      alert(response.message);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <form id="register-form" onSubmit={handleSubmit}>
+        <div className="form-box register">
+          <h2>Registration</h2>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="person"></ion-icon>
+            </span>
+            <input
+              type="text"
+              required
+              title="Enter Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label>Username</label>
           </div>
-        </form>
-        
-      </div>
-    );
-    
-  }
-  
-  export default RegisterForm;
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="mail"></ion-icon>
+            </span>
+            <input
+              type="email"
+              required
+              title="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Email</label>
+          </div>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="lock-closed"></ion-icon>
+            </span>
+            <input
+              type="password"
+              required
+              title="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label>Password</label>
+          </div>
+          <button type="submit" className="btn">
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterForm;
